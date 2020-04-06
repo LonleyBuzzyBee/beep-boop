0a1525887a4c105aea3be77a7f869ac686f1ce94
mai cook <mailiecook@yahoo.com>
2020-04-05 20:04:07 -0700

    debugging scripts.js

--- js/script.js
+++ js/script.js
@@ -8,16 +8,20 @@ function myFunc(numbers) {
     
  }
 function myFunc2(stringArray) {
-  var string1 = stringArray.join(" ");
-  var numberReplace = ["1","2","3"]
-  var numb1 = /1/gi;
-  console.log(string1);
+  var numberReplace = ["1","2","3"];
+  var output = [];
+  
+  if (stringArray.includes(numberReplace[0])) {
+      output.push("beep");
+      $("#result").text(output);
+  }else if (stringArray.includes(numberReplace[1])) {
+      output.push("boop");
+      $("#result").text(output);
+  }else if (stringArray.includes(numberReplace[2])){
+      output.push("Won't you be my neighbor?");
+      $("#result").text(output);
+  }else{
 
-  if (string1.includes(numberReplace[0])) {
-      $("#result").text(string1.replace(numb1,"beep"));
-  }else {
-      // stringArray.push();
-    
   }
 }
 
@@ -34,8 +38,9 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
      var inputNumbers = $("input").val();
-     var output = [];
+    //  var output = [];
      myFunc(inputNumbers);
     //  $("#result").text(output);
+    //  console.log(output);
   });
 });
