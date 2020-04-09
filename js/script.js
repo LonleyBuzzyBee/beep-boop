cefd79250d96a5671a8d07ec6cf41e580785ec9d
mai cook <mailiecook@yahoo.com>
2020-04-09 13:06:27 -0700

    debugging code

--- js/script.js
+++ js/script.js
@@ -1,46 +1,61 @@
-function myFunc(numbers) {
-  var stringArray = numbers.split("").map(function(string) {
-    return string.toString();
-  
-  });
 
-    myFunc2(stringArray);
-    
- }
-function myFunc2(stringArray) {
-  var numberReplace = ["1","2","3"];
-  var output = [];
+function myFunc(numberInput) {
   
-  if (stringArray.includes(numberReplace[0])) {
-      output.push("beep");
-      $("#result").text(output);
-  }else if (stringArray.includes(numberReplace[1])) {
-      output.push("boop");
-      $("#result").text(output);
-  }else if (stringArray.includes(numberReplace[2])){
-      output.push("Won't you be my neighbor?");
-      $("#result").text(output);
-  }else{
-
+  for (var i = 0; i <= numberInput; i++) {
   }
-}
+
+  console.log(i);
+  }
+
+     
+ 
+  // const output = [];
+  // var outputJoin = output.join(" ");
+  // var numberReplace = ["1","2","3"]; 
+  // var numberArray = numberInput.split("").map(function(number) {
+  //   return number.toString;
+
+  // });
+  //   $(".jumbotron").show();
+  //   $("#result").html(outputJoin);
 
 
+  // console.log(outputJoin);
 
 
+//   if (numberArray.includes(numberReplace[2])) {
+//       output.push("Won't you be my neighbor?");
+     
+//   }else if (numberArray.includes(numberReplace[1])) {
+//       output.push("boop");
+  
+//   }else if (numberArray.includes(numberReplace[0])){
+//       output.push("beep");
+    
+//   }else{
+//       output.push(numberString);
+//   }
 
+// }
 
-
+// function printFunc(outputJoin) {
+     
+//     }
 
 
 // user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-     var inputNumbers = $("input").val();
-    //  var output = [];
+     var inputNumbers = parseInt($("#count").val());
+   
+    // for (var i = 0; i <= inputNumbers; i++) {
+    // }
+
      myFunc(inputNumbers);
-    //  $("#result").text(output);
-    //  console.log(output);
+
+     $(".jumbotron").show();
+     $("#result").html(inputNumbers);
+    
   });
 });
