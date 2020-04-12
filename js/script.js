14d38c3ed3836b9d0b8dfb6bb7f1b3355bdc3e2c
mai cook <mailiecook@yahoo.com>
2020-04-11 18:53:11 -0700

    made break through pushing i into an array scripts2.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ var numberArray = numberInput.split("").map(function(number) {
      
  
   // const output = [];
-  // var outputJoin = output.join(" ");
+  var outputJoin = output.join(" ");
   // var numberReplace = ["1","2","3"]; 
   
     
@@ -25,20 +25,20 @@ var numberArray = numberInput.split("").map(function(number) {
   // console.log(outputJoin);
 
 
-//   if (numberArray.includes(numberReplace[2])) {
-//       output.push("Won't you be my neighbor?");
+  if (numberArray.includes(numberReplace[2])) {
+      output.push("Won't you be my neighbor?");
      
-//   }else if (numberArray.includes(numberReplace[1])) {
-//       output.push("boop");
+  }else if (numberArray.includes(numberReplace[1])) {
+      output.push("boop");
   
-//   }else if (numberArray.includes(numberReplace[0])){
-//       output.push("beep");
+  }else if (numberArray.includes(numberReplace[0])){
+      output.push("beep");
     
-//   }else{
-//       output.push(numberString);
-//   }
+  }else{
+      output.push(numberString);
+  }
 
-// }
+}
 
 // function printFunc(outputJoin) {
      
--- js/scripts2.js
+++ js/scripts2.js
@@ -1,32 +1,26 @@
+function myFunc(index) {
+  console.log(index);
+  
+  
+  
+}
 
-// function myFunc(numberInput) {
-//   const output = [];
 
 
 // user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-     var inputNumbers = $("#count").val();
-     var stringArray = inputNumbers.split(" ");
-     console.log(stringArray);
-     var numberArray = stringArray.map(_=>_|0);
-    console.log(numberArray)
-    for (var i = 0; i <= numberArray[0].lenght; i++) {
+    var arr = [];
+    var inputNumbers = $("#count").val();
+    for (var i = 0; i <= inputNumbers; i++){
+       arr.push(i)
     }
-      console.log(i);
-      console.log(Math.floor(numberArray[0]));
-    // for ( var i = inputNumbers++ ) {
-      //  console.log(numberArray);
-    // }
-    // var numberArray = output.map(function(number) {
-    //   console.log(numberArray);
-    //   return number.toString();
-    // });
-    //  myFunc(i);
-
-     $(".jumbotron").show();
-     $("#result").html(i);
+    
+    myFunc(arr);
+    
+    $(".jumbotron").show();
+    $("#result").html(arr);
     
   });
 });
