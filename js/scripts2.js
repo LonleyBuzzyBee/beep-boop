a4a62ad044bda994c000496af113ef8ee16b4250
mai cook <mailiecook@yahoo.com>
2020-04-13 10:58:21 -0700

    adding changes

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,9 +1,48 @@
 function myFunc(index) {
+  // var index = index1.join(" ");
+
   console.log(index);
+  var numberReplace = {["3"]:"Won't you be my neighbor?",["2"]:"boop",["1"]:"beep"};
   
+  // var output = index.join(" ");
   
-  
+// index.forEach(function(element) { 
+//   element.slice("3")
+//   console.log(element.slice("3"));
+// })
+index = index.replace(/[3,2,1]/g, r => numberReplace [r]);
+$("#result").text(index);
 }
+  
+  
+    // if (index.includes(numberReplace[2]) && index.includes(numberReplace[1]) && index.includes(numberReplace[0])) {
+    // index.splice(3,1,"Won't you be my neighbor?"); 
+    // index.splice(2,1,"boop");
+    // index.splice(1,1,"beep");
+  
+    // $("#result").text(index);
+
+   
+// }else if (index.includes(numberReplace[2]) && index.includes(numberReplace[1]) && index.includes(numberReplace[0]))) {
+//     index.splice(2,1,"boop");
+//     $("#result").text(index);
+
+// }else if (index.includes(numberReplace[0])){
+//     index.splice(1,1,"beep");
+//     $("#result").text(index);
+  
+// }else{
+//     index.push(index);
+//     $("#result").text(index);
+  
+// }
+// helpF(index);
+// }
+  
+//   function helpF(newArr) {
+//   console.log(newArr);
+
+// }
 
 
 
@@ -14,13 +53,24 @@ $(document).ready(function() {
     var arr = [];
     var inputNumbers = $("#count").val();
     for (var i = 0; i <= inputNumbers; i++){
-       arr.push(i)
-    }
+  
+        arr.push(i);
     
-    myFunc(arr);
+      
+    } 
+    var arrCombine = arr.join(" ");
     
-    $(".jumbotron").show();
-    $("#result").html(arr);
+    
+   
+     myFunc(arrCombine);
+ console.log(arr);
+       
+
+     
+   
+      $(".jumbotron").show();
+      // $("#result").html();
+
     
   });
 });
