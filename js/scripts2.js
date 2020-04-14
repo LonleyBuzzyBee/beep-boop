5a3b7c14898b8e4aff0d213c1bf5dd51077565d0
mai cook <mailiecook@yahoo.com>
2020-04-14 08:07:54 -0700

    project complete

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,75 +1,42 @@
-function myFunc(index) {
-  // var index = index1.join(" ");
 
-  console.log(index);
-  var numberReplace = {["3"]:"Won't you be my neighbor?",["2"]:"boop",["1"]:"beep"};
-  
-  // var output = index.join(" ");
-  
-// index.forEach(function(element) { 
-//   element.slice("3")
-//   console.log(element.slice("3"));
-// })
-index = index.replace(/[3,2,1]/g, r => numberReplace [r]);
-$("#result").text(index);
+
+function sort(inputNumbers) {
+  console.log(inputNumbers);
+
+  var arr = [];
+
+  for (var i = 0; i <= inputNumbers; i++){
+     
+    if ( i.toString().includes("3") ) {
+     
+       arr.push("Won't you be my neighbor?");
+     
+      } else if ( i.toString().includes("2") ) {
+      
+      arr.push("boop");
+
+      } else if ( i.toString().includes("1") ) {
+      
+      arr.push("beep");
+
+      // console.log(arr.push(i));
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr.join (" ");
 }
-  
-  
-    // if (index.includes(numberReplace[2]) && index.includes(numberReplace[1]) && index.includes(numberReplace[0])) {
-    // index.splice(3,1,"Won't you be my neighbor?"); 
-    // index.splice(2,1,"boop");
-    // index.splice(1,1,"beep");
-  
-    // $("#result").text(index);
-
-   
-// }else if (index.includes(numberReplace[2]) && index.includes(numberReplace[1]) && index.includes(numberReplace[0]))) {
-//     index.splice(2,1,"boop");
-//     $("#result").text(index);
-
-// }else if (index.includes(numberReplace[0])){
-//     index.splice(1,1,"beep");
-//     $("#result").text(index);
-  
-// }else{
-//     index.push(index);
-//     $("#result").text(index);
-  
-// }
-// helpF(index);
-// }
-  
-//   function helpF(newArr) {
-//   console.log(newArr);
-
-// }
-
-
 
 // user interface logic
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var arr = [];
     var inputNumbers = $("#count").val();
-    for (var i = 0; i <= inputNumbers; i++){
-  
-        arr.push(i);
-    
-      
-    } 
-    var arrCombine = arr.join(" ");
-    
-    
-   
-     myFunc(arrCombine);
- console.log(arr);
-       
-
-     
-   
+    var result = sort(inputNumbers);
+   console.log(result);
       $(".jumbotron").show();
-      // $("#result").html();
+      $("#result").text(result);
 
     
   });
---
+++ readme.md
@@ -0,0 +1,66 @@
+# _Beep Boop_
+
+#### _ A program designed to count up to a submitted number and replace the numbers 1 2 and 3 with phrases. version 1 latest update 2020 _
+
+#### By _**Mai .C**_
+
+
+
+
+## Description
+
+_This webpage was created to help entertain the user by having them input a number then having the program count up to the submited number but replacing key numbers with fun phrases._
+
+## Specifications:
+| Behavior | Input | Output |
+|---|---|---|
+| Spec: The program returns a range of numbers from 0 to the users inputted number | 7 | 0,1,2,3,4,5,6,7 |
+| Then it replaces every number with the value 1 with beep! | 10 | 0,beep!,2,3,4,5,6,7,8,9,beep!  |
+| Then it replaces every number with the value 2 with boop!| 2 | 0,1,boop!|
+|Then it replaces every number with the value 3 with Won't you be my neighbor?| 3 | 0,1,2,Won't you be my neighbor?
+| then all of these properties are applied, 3 being the most important | 23  | 0,beep!,boop!,Won't you be my neighbor?,4,5,6,7,8,9,beep!,beep!,boop!,Won't you be my neighbor?,beep!,beep!,beep!,beep!,beep!,beep!,boop!,boop!,boop!,Won't you be my neighbor?.|
+---
+
+## Setup/Installation Requirements
+_In Terminal:_
+
+* Navigate to where you want this application to be saved, i.e.:
+```cd desktop```
+* Clone the file from GitHub with HTTPS
+```git clone https://github.com/LonleyBuzzyBee/beep-boop.git```
+* Open file in your preferred text editor
+* On Mac: ```open -a {your text editor} pizza--parlor```
+* On Windows: ```beep-boop```
+
+_To Download Manually:_
+
+* Navigate to https://github.com/LonleyBuzzyBee/beep-boop
+* Click green "Clone or Download" button.
+* Click "Download ZIP".
+* Click downloaded file to unzip.
+* Open folder called "beep-boop-master".
+* Right click "index.html" and select your preferred browser or text editor.
+
+## Known Bugs
+
+_No known bugs at this time._
+
+## Support and contact details
+
+_Have a bug or an issue with this application? [Open a new issue](https://github.com/LonleyBuzzyBee/beep-boop/issues) here on GitHub._
+
+## Technologies Used
+
+_*CSS_
+_*HTML_
+_*MarkDown_
+_*Javascript_
+_*Bootstrap version 4_
+_*Jquery_
+
+
+### License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+Copyright (c) 2020 **_Mai C._**
